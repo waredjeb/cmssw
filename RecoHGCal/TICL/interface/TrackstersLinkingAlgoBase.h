9262c94440757213e7a69b7d1373027bd56b9404
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2021-06-23 10:53:49 +0200

    fix virtual function definition, modify Inputs datastructure adding tracks

--- RecoHGCal/TICL/interface/TrackstersLinkingAlgoBase.h
+++ RecoHGCal/TICL/interface/TrackstersLinkingAlgoBase.h
@@ -6,6 +6,7 @@
 #include "DataFormats/CaloRecHit/interface/CaloCluster.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "DataFormats/HGCalReco/interface/Trackster.h"
+#include "DataFormats/TrackReco/interface/Track.h"
 #include "RecoHGCal/TICL/interface/GlobalCache.h"
 
 namespace edm {
@@ -22,15 +23,17 @@ namespace ticl {
     struct Inputs {
       const edm::Event& ev;
       const edm::EventSetup& es;
-      const std::vector<reco::CaloCluster>& layerClusters;
       const std::vector<Trackster>& tracksters;
+      const std::vector<reco::Track>& trks;
 
       Inputs(const edm::Event& eV,
              const edm::EventSetup& eS,
-             const std::vector<reco::CaloCluster>& lC,
-             const std::vector<Trackster>& ts)
-          : ev(eV), es(eS), layerClusters(lC), tracksters(ts) {}
+             const std::vector<Trackster>& ts,
+             const std::vector<reco::Track>& tracks)
+          : ev(eV), es(eS), tracksters(ts), trks(tracks) {}
     };
+
+    virtual void trackstersInfo(const Inputs& input) = 0;
   };
 }  // namespace ticl
 
