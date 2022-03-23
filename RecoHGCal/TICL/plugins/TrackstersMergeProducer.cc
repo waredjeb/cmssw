44bf44529c0865a537d800644ea45ed50bb28d79
Abhirikshma <abhirikshma14@gmail.com>
2022-03-23 12:29:35 +0100

    TICLV4 geometric linking
    updating linking configuration
    
    Moving CLUE3D consume in the ticlv4 code
    
    Disable TICLv4 by default
    
    require all the energy to be em to call a trackster em
    
    require all the energy to be em to call a trackster em
    
    Run PCA on TICLv4 tracksters
    
    clang format
    
    do not create trackster for trackster-less ticl candidate
    
    remove PCA propagation, fall back to barycenter
    
    make sets complementary

--- RecoHGCal/TICL/plugins/TrackstersMergeProducer.cc
+++ RecoHGCal/TICL/plugins/TrackstersMergeProducer.cc
@@ -1,4 +1,6 @@
 #include <memory>  // unique_ptr
+#include <ostream>
+
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
@@ -46,7 +48,14 @@
 #include "Geometry/Records/interface/IdealGeometryRecord.h"
 #include "Geometry/CommonDetUnit/interface/GeomDet.h"
 
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "TTree.h"
+#include "TH2D.h"
+
 #include "TrackstersPCA.h"
+#include "SeedingRegionByTracks.h"
 
 using namespace ticl;
 
@@ -100,6 +109,7 @@ private:
   const edm::ESGetToken<MagneticField, IdealMagneticFieldRecord> bfield_token_;
   const edm::ESGetToken<Propagator, TrackingComponentsRecord> propagator_token_;
   const bool optimiseAcrossTracksters_;
+  const bool ticlv4_;
   const int eta_bin_window_;
   const int phi_bin_window_;
   const double pt_sigma_high_;
@@ -157,6 +167,7 @@ TrackstersMergeProducer::TrackstersMergeProducer(const edm::ParameterSet &ps)
       propagator_token_(
           esConsumes<Propagator, TrackingComponentsRecord, edm::Transition::BeginRun>(edm::ESInputTag("", propName_))),
       optimiseAcrossTracksters_(ps.getParameter<bool>("optimiseAcrossTracksters")),
+      ticlv4_(ps.getParameter<bool>("TICLV4")),
       eta_bin_window_(ps.getParameter<int>("eta_bin_window")),
       phi_bin_window_(ps.getParameter<int>("phi_bin_window")),
       pt_sigma_high_(ps.getParameter<double>("pt_sigma_high")),
@@ -180,6 +191,7 @@ TrackstersMergeProducer::TrackstersMergeProducer(const edm::ParameterSet &ps)
       eidMinClusterEnergy_(ps.getParameter<double>("eid_min_cluster_energy")),
       eidNLayers_(ps.getParameter<int>("eid_n_layers")),
       eidNClusters_(ps.getParameter<int>("eid_n_clusters")),
+      cutTk_(ps.getParameter<std::string>("cutTk")),
       eidSession_(nullptr) {
   produces<std::vector<Trackster>>();
   produces<std::vector<TICLCandidate>>();
@@ -578,6 +590,7 @@ void TrackstersMergeProducer::fillDescriptions(edm::ConfigurationDescriptions &d
   desc.add<std::string>("detector", "HGCAL");
   desc.add<std::string>("propagator", "PropagatorWithMaterial");
   desc.add<bool>("optimiseAcrossTracksters", true);
+  desc.add<bool>("TICLV4", false);
   desc.add<int>("eta_bin_window", 1);
   desc.add<int>("phi_bin_window", 1);
   desc.add<double>("pt_sigma_high", 2.);
