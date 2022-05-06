62b3c9cd58537d2f3c78d3d710f61a832c2ce735
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2022-05-06 15:43:47 +0200

    save clue3d

--- RecoHGCal/TICL/plugins/TrackstersProducer.cc
+++ RecoHGCal/TICL/plugins/TrackstersProducer.cc
@@ -110,6 +110,7 @@ TrackstersProducer::TrackstersProducer(const edm::ParameterSet& ps)
 
   produces<std::vector<Trackster>>();
   produces<std::vector<float>>();  // Mask to be applied at the next iteration
+  produces<std::vector<int>>("tracksterSeeds");
 }
 
 void TrackstersProducer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
@@ -148,6 +149,7 @@ void TrackstersProducer::fillDescriptions(edm::ConfigurationDescriptions& descri
 
 void TrackstersProducer::produce(edm::Event& evt, const edm::EventSetup& es) {
   auto result = std::make_unique<std::vector<Trackster>>();
+  auto tracksterSeeds = std::make_unique<std::vector<int>>();
   auto output_mask = std::make_unique<std::vector<float>>();
 
   const std::vector<float>& original_layerclusters_mask = evt.get(original_layerclusters_mask_token_);
@@ -180,7 +182,8 @@ void TrackstersProducer::produce(edm::Event& evt, const edm::EventSetup& es) {
                                                                                          tfSession_,
                                                                                          tfSessionER_);
 
-    myAlgoHFNose_->makeTracksters(inputHFNose, *result, seedToTrackstersAssociation);
+    typename PatternRecognitionAlgoBaseT<TICLLayerTilesHFNose>::Outputs output(*result, *tracksterSeeds);
+    myAlgoHFNose_->makeTracksters(inputHFNose, output, seedToTrackstersAssociation);
 
   } else {
     const auto& layer_clusters_tiles = evt.get(layer_clusters_tiles_token_);
@@ -194,7 +197,8 @@ void TrackstersProducer::produce(edm::Event& evt, const edm::EventSetup& es) {
                                                                              tfSession_,
                                                                              tfSessionER_);
 
-    myAlgo_->makeTracksters(input, *result, seedToTrackstersAssociation);
+   typename PatternRecognitionAlgoBaseT<TICLLayerTiles>::Outputs output(*result, *tracksterSeeds);
+    myAlgo_->makeTracksters(input, output, seedToTrackstersAssociation);
   }
   // Now update the global mask and put it into the event
   output_mask->reserve(original_layerclusters_mask.size());
@@ -214,4 +218,5 @@ void TrackstersProducer::produce(edm::Event& evt, const edm::EventSetup& es) {
 
   evt.put(std::move(result));
   evt.put(std::move(output_mask));
+  evt.put(std::move(tracksterSeeds), "tracksterSeeds");
 }
