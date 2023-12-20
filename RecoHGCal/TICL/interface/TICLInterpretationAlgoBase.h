b3f69e2ce0c58b5cee699f69860b25497004ec8f
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2023-12-20 12:41:31 +0100

    ticlv5: clang-format + remove ESConsumesCollector

--- RecoHGCal/TICL/interface/TICLInterpretationAlgoBase.h
+++ RecoHGCal/TICL/interface/TICLInterpretationAlgoBase.h
@@ -15,7 +15,6 @@
 #include "DataFormats/Common/interface/ValueMap.h"
 #include "RecoHGCal/TICL/interface/GlobalCache.h"
 #include "RecoHGCal/TICL/interface/commons.h"
-#include "FWCore/Framework/interface/ESConsumesCollector.h"
 #include "FWCore/Framework/interface/ConsumesCollector.h"
 #include "PhysicsTools/TensorFlow/interface/TensorFlow.h"
 #include "CommonTools/RecoAlgos/interface/MultiVectorManager.h"
@@ -25,10 +24,10 @@ namespace edm {
   class EventSetup;
 }  // namespace edm
 namespace ticl {
-  template<typename T>
+  template <typename T>
   class TICLInterpretationAlgoBase {
   public:
-    TICLInterpretationAlgoBase(const edm::ParameterSet& conf, edm::ConsumesCollector, edm::ESConsumesCollector)
+    TICLInterpretationAlgoBase(const edm::ParameterSet& conf, edm::ConsumesCollector)
         : algo_verbosity_(conf.getParameter<int>("algo_verbosity")) {}
     virtual ~TICLInterpretationAlgoBase(){};
     struct Inputs {
@@ -37,20 +36,27 @@ namespace ticl {
       const std::vector<reco::CaloCluster>& layerClusters;
       const edm::ValueMap<std::pair<float, float>>& layerClustersTime;
       const MultiVectorManager<Trackster>& tracksters;
-      const std::vector<std::vector<unsigned int>> & linkedResultTracksters;
+      const std::vector<std::vector<unsigned int>>& linkedResultTracksters;
       const std::vector<T>& tracks;
-      
+
       Inputs(const edm::Event& eV,
              const edm::EventSetup& eS,
              const std::vector<reco::CaloCluster>& lC,
              const edm::ValueMap<std::pair<float, float>>& lT,
              const MultiVectorManager<Trackster>& tS,
-             const std::vector<std::vector<unsigned int>> & links,
-             const std::vector<T>& trks )
-          : ev(eV), es(eS), layerClusters(lC), layerClustersTime(lT), tracksters(tS), linkedResultTracksters(links), tracks(trks) {}
+             const std::vector<std::vector<unsigned int>>& links,
+             const std::vector<T>& trks)
+          : ev(eV),
+            es(eS),
+            layerClusters(lC),
+            layerClustersTime(lT),
+            tracksters(tS),
+            linkedResultTracksters(links),
+            tracks(trks) {}
     };
 
-    virtual void makeCandidates(const Inputs& input, std::vector<Trackster>& resultTracksters,
+    virtual void makeCandidates(const Inputs& input,
+                                std::vector<Trackster>& resultTracksters,
                                 std::vector<TICLCandidate>& resultCandidate) = 0;
 
     static void fillPSetDescription(edm::ParameterSetDescription& desc) { desc.add<int>("algo_verbosity", 0); };
