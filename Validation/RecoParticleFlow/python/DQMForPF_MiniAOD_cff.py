0fad9adb8db611930812dbb5b844a2c2041b9b20
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2024-03-08 09:48:10 +0100

    add modules for performing pf validation at hlt

--- Validation/RecoParticleFlow/python/DQMForPF_MiniAOD_cff.py
+++ Validation/RecoParticleFlow/python/DQMForPF_MiniAOD_cff.py
@@ -1,11 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
-from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetAnalyzerDQM
+from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetAnalyzerDQM,pfJetAnalyzerHLTDQM 
+#from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetAnalyzerDQM
 from Validation.RecoParticleFlow.particleFlowDQM_cff import pfPuppiJetAnalyzerDQM
 from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetDQMPostProcessor
 from Validation.RecoParticleFlow.particleFlowDQM_cff import PFCandAnalyzerDQM
 from Validation.RecoParticleFlow.offsetAnalyzerDQM_cff import offsetAnalyzerDQM
+from Validation.RecoParticleFlow.particleFlowDQM_cff import PFCandAnalyzerHLTDQM
 from Validation.RecoParticleFlow.offsetAnalyzerDQM_cff import offsetDQMPostProcessor
+#from Validation.RecoParticleFlow.pfCaloGPUComparisonTask_cfi import HLTpfClusterHBHEAlpakaComparison
+from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetHLTDQMPostProcessor
 # Use also other POGs' analyzers for extended checks
 from Validation.RecoMET.METRelValForDQM_cff import *
 from Validation.RecoJets.JetValidation_cff import *
@@ -27,3 +31,14 @@ DQMOfflinePFExtended = cms.Sequence(
     METValidationMiniAOD +
     JetValidationMiniAOD
 )
+
+
+DQMHLTPF = cms.Sequence(
+    pfJetAnalyzerHLTDQM+
+    PFCandAnalyzerHLTDQM
+#    HLTpfClusterHBHEAlpakaComparison
+)
+
+DQMHarvestHLTPF = cms.Sequence(
+  pfJetHLTDQMPostProcessor
+  )
