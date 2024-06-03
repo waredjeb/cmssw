538cfa3b15495a3ce0e2fce3efa9943358c4fffb
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2024-06-03 10:27:18 +0200

    DQM for PF

--- Validation/RecoParticleFlow/python/DQMForPF_MiniAOD_cff.py
+++ Validation/RecoParticleFlow/python/DQMForPF_MiniAOD_cff.py
@@ -1,14 +1,19 @@
 import FWCore.ParameterSet.Config as cms
 
-from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetAnalyzerDQM
+from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetAnalyzerDQM,pfJetAnalyzerHLTDQM 
+#from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetAnalyzerDQM
 from Validation.RecoParticleFlow.particleFlowDQM_cff import pfPuppiJetAnalyzerDQM
 from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetDQMPostProcessor
 from Validation.RecoParticleFlow.particleFlowDQM_cff import PFCandAnalyzerDQM
 from Validation.RecoParticleFlow.offsetAnalyzerDQM_cff import offsetAnalyzerDQM
+from Validation.RecoParticleFlow.particleFlowDQM_cff import PFCandAnalyzerHLTDQM
 from Validation.RecoParticleFlow.offsetAnalyzerDQM_cff import offsetDQMPostProcessor
+#from DQM.ParticleFlow.pfCaloGPUComparisonTask_cfi import HLTpfClusterHBHEAlpakaComparisonGPUvsCPU#, HLTpfClusterHBHEAlpakaComparisonCPUvsLegacy, HLTpfClusterHBHEAlpakaComparisonGPUvsLegacy
+from Validation.RecoParticleFlow.particleFlowDQM_cff import pfJetHLTDQMPostProcessor
 # Use also other POGs' analyzers for extended checks
 from Validation.RecoMET.METRelValForDQM_cff import *
 from Validation.RecoJets.JetValidation_cff import *
+
 
 DQMOfflinePF = cms.Sequence(
   pfJetAnalyzerDQM +
@@ -27,3 +32,16 @@ DQMOfflinePFExtended = cms.Sequence(
     METValidationMiniAOD +
     JetValidationMiniAOD
 )
+
+
+DQMHLTPF = cms.Sequence(
+#    pfJetAnalyzerHLTDQM+
+    PFCandAnalyzerHLTDQM
+    #HLTpfClusterHBHEAlpakaComparisonGPUvsCPU
+    
+)
+
+DQMHarvestHLTPF = cms.Sequence(
+  pfJetHLTDQMPostProcessor
+  )
+
