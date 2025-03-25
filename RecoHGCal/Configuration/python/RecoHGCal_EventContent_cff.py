ea70b75bde8d58115c7b6a72e5a9822292c72d1e
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-03-25 20:09:44 +0100

    add hltTiclEgammSuperClusters to HLT Event Content

--- RecoHGCal/Configuration/python/RecoHGCal_EventContent_cff.py
+++ RecoHGCal/Configuration/python/RecoHGCal_EventContent_cff.py
@@ -89,6 +89,10 @@ TICLv5_FEVTHLT = cms.PSet(
             'keep *_hltTiclTracksterLinks_*_*',
             'keep *_hltTiclCandidate_*_*',
             'keep *_hltPfTICL_*_*',
+            'keep *_hltTiclEGammaSuperClusterProducerUnseeded_*_*',
+            'keep *_hltTiclTracksterLinksSuperclusteringDNNUnseeded_*_*'
+            'keep *_hltTiclEGammaSuperClusterProducerL1Seeded_*_*',
+            'keep *_hltTiclTracksterLinksSuperclusteringDNNL1Seeded_*_*'
         ]
     )
 )
