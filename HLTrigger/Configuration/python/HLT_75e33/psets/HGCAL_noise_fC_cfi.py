9bfb0e965ae6eb115879488823e6ac4535d7d45a
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-06-11 15:28:59 +0200

    fix more paramters through V19

--- HLTrigger/Configuration/python/HLT_75e33/psets/HGCAL_noise_fC_cfi.py
+++ HLTrigger/Configuration/python/HLT_75e33/psets/HGCAL_noise_fC_cfi.py
@@ -1,9 +1,12 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.Eras.Modifier_phase2_hgcalV19_cff import phase2_hgcalV19
 
 HGCAL_noise_fC = cms.PSet(
     doseMap = cms.string(''),
     scaleByDose = cms.bool(False),
     scaleByDoseAlgo = cms.uint32(0),
     scaleByDoseFactor = cms.double(1),
-    values = cms.vdouble(0.32041011999999996, 0.384492144, 0.32041011999999996, 0.384492144)
+    values = cms.vdouble(0.32041011999999996, 0.384492144, 0.32041011999999996)
 )
+
+phase2_hgcalV19.toModify(HGCAL_noise_fC , values = cms.vdouble(0.32041011999999996, 0.384492144, 0.32041011999999996, 0.384492144))
