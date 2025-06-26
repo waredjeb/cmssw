304a0bbb2c3d98ce0ebbdac7cbbfcfe5a25f690a
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-06-26 21:07:55 +0200

    TracksterSoA collections and TracksterSoAProducer

---
+++ DataFormats/HGCalReco/interface/TracksterSoAHostCollection.h
@@ -0,0 +1,9 @@
+#ifndef DataFormats_HGCalReco_interface_TrackstersSoAHostCollection_h
+#define DataFormats_HGCalReco_interface_TrackstersSoAHostCollection_h
+
+#include "DataFormats/Portable/interface/PortableHostCollection.h"
+#include "DataFormats/HGCalReco/interface/TrackstersSoA.h"
+
+using TrackstersSoAHostCollection = PortableHostCollection<TrackstersSoA>;
+
+#endif  // DataFormats_HGCalReco_interface_TrackstersSoAHostCollection_h
---
+++ DataFormats/HGCalReco/interface/alpaka/TracksterSoADeviceCollection.h
@@ -0,0 +1,14 @@
+#ifndef DataFormats_PortableTestObjects_interface_alpaka_TrackstersSoADeviceCollection_h
+#define DataFormats_PortableTestObjects_interface_alpaka_TrackstersSoADeviceCollection_h
+
+#include "DataFormats/Portable/interface/alpaka/PortableCollection.h"
+#include "DataFormats/HGCalReco/interface/TrackstersSoA.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  using TrackstersSoADeviceCollection = PortableCollection<TrackstersSoA>;
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+#endif  // DataFormats_PortableTestObjects_interface_alpaka_TrackstersSoADeviceCollection_h
