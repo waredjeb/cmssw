74dbca006bbb9b413d41a8b1e7b9b5b155ab22df
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-07-02 17:24:45 +0200

    add TICLCandidates to NanoAOD

--- HLTrigger/NGTScouting/python/HLTNanoProducer_cff.py
+++ HLTrigger/NGTScouting/python/HLTNanoProducer_cff.py
@@ -13,6 +13,7 @@ from HLTrigger.NGTScouting.hltTracks_cfi import *
 from HLTrigger.NGTScouting.hltJets_cfi import *
 from HLTrigger.NGTScouting.hltTaus_cfi import *
 from HLTrigger.NGTScouting.hltTracksters_cfi import *
+from HLTrigger.NGTScouting.hltTICLCandidates_cfi import *
 from HLTrigger.NGTScouting.hltCaloParticles_cfi import *
 from HLTrigger.NGTScouting.hltSums_cfi import *
 from HLTrigger.NGTScouting.hltTriggerAcceptFilter_cfi import hltTriggerAcceptFilter,dstTriggerAcceptFilter
@@ -53,6 +54,7 @@ hltNanoProducer = cms.Sequence(
     + hltPFCandidateTable
     + hltJetTable
     + trackstersSeq 
+    + hltTiclCandidateTable 
     + hltTauTable
     + hltTauExtTable
     + METTable
@@ -76,6 +78,7 @@ dstNanoProducer = cms.Sequence(
     + hltJetTable
     + hltTauTable
     + trackstersSeq 
+    + hltTiclCandidateTable 
     + hltTauExtTable
     + METTable
     + HTTable
