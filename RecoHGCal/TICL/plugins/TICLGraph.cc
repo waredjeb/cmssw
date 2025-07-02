ae5eed867fc97ae6ffb3d68c0319ab5f43a8c741
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-07-02 12:17:55 +0200

    fix multicollection class definition

--- RecoHGCal/TICL/plugins/TICLGraph.cc
+++ RecoHGCal/TICL/plugins/TICLGraph.cc
@@ -64,12 +64,12 @@ inline void TICLGraph::findRootNodes() {
   }
 }
 
-// size_t TICLGraph::getNumberOfEdges() const {
-//   size_t edges = 0;
-//   for (auto const& n : nodes_)
-//     edges += n.getOuterNeighbours().size();
-//   return edges;
-// }
+size_t TICLGraph::getNumberOfEdges() const {
+  size_t edges = 0;
+  for (auto const& n : nodes_)
+    edges += n.getOuterNeighbours().size();
+  return edges;
+}
 
 bool TICLGraph::isGraphOk() {
   for (const auto& n : nodes_) {
