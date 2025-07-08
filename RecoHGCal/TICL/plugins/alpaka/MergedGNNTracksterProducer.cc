a3860118900909475cc835f404df11894249a514
Christine Zeh <zeh.chrisi@gmail.com>
2025-07-08 15:54:38 +0200

    Add post processing producer; WIP

---
+++ RecoHGCal/TICL/plugins/alpaka/MergedGNNTracksterProducer.cc
@@ -0,0 +1,76 @@
+#include "DataFormats/CaloRecHit/interface/CaloCluster.h"
+#include "DataFormats/HGCalReco/interface/alpaka/TracksterSoADeviceCollection.h"
+#include "DataFormats/HGCalReco/interface/TracksterSoAHostCollection.h"
+#include "DataFormats/HGCalReco/interface/Trackster.h"
+#include "DataFormats/HGCalReco/interface/TICLGraph.h"
+#include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/EDPutToken.h"
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/Event.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/EventSetup.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/MakerMacros.h"
+#include "HeterogeneousCore/AlpakaCore/interface/alpaka/stream/EDProducer.h"
+#include "HeterogeneousCore/AlpakaInterface/interface/config.h"
+#include "PhysicsTools/PyTorch/interface/Nvtx.h"
+
+namespace ALPAKA_ACCELERATOR_NAMESPACE {
+
+  class MergedGNNTracksterProducer : public stream::EDProducer<> {
+  public:
+    float detector_size = (2*(3 - 1.5) * (2 * 47));
+    MergedGNNTracksterProducer(const edm::ParameterSet &params);
+
+    void produce(device::Event &event, const device::EventSetup &event_setup) override;
+    static void fillDescriptions(edm::ConfigurationDescriptions &descriptions);
+
+  private:
+    const edm::EDGetTokenT<std::vector<ticl::Trackster>> tracksters_token_;
+    const edm::EDGetTokenT<TrackstersGNNOutputSoADeviceCollection> gnn_output_token_;
+    const edm::EDPutTokenT<std::vector<ticl::Trackster>> merged_tracksters_token_; /**< Token to store output data. */
+  };
+
+  MergedGNNTracksterProducer::MergedGNNTracksterProducer(edm::ParameterSet const &params)
+      : EDProducer<>(params),
+        tracksters_token_(consumes<std::vector<ticl::Trackster>>(params.getParameter<edm::InputTag>("tracksters"))),
+        gnn_output_token_(consumes<TrackstersGNNOutputSoADeviceCollection>(params.getParameter<edm::InputTag>("gnnOutput"))),
+        merged_tracksters_token_{produces()} {}
+
+  void MergedGNNTracksterProducer::produce(device::Event &event, const device::EventSetup &event_setup) {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    auto const& gnn_output = event.get(gnn_output_token_);
+    auto const& tracksters = event.get(tracksters_token_);
+
+    // debug stream usage in concurrently scheduled modules
+    std::stringstream msg_stream;
+    msg_stream << "MergedGNNTracksterProducer::produce [E: " << event.id().event() << "]";
+    auto msg = msg_stream.str();
+    NvtxScopedRange produce_range(msg.c_str());
+
+
+    std::vector<ticl::Trackster> output;
+    event.emplace(merged_tracksters_token_, std::move(output));
+
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::cout << "(MergedGNNTracksterProducer) E: " << event.id().event() << " OK - "
+              << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() << " us" << std::endl;
+    produce_range.end();
+  }
+
+  /**
+   * @brief Describes the allowed configuration parameters for this module.
+   * @param descriptions Configuration description object to populate.
+   */
+  void MergedGNNTracksterProducer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
+    edm::ParameterSetDescription desc;
+    desc.add<edm::InputTag>("tracksters", edm::InputTag("ticlTrackstersCLUE3DHigh"));
+    desc.add<edm::InputTag>("gnnOutput", edm::InputTag("ticlTrackstersLinkingByGNNProducer"));
+    descriptions.addWithDefaultLabel(desc);
+  }
+
+}  // namespace ALPAKA_ACCELERATOR_NAMESPACE
+
+DEFINE_FWK_ALPAKA_MODULE(MergedGNNTracksterProducer);
