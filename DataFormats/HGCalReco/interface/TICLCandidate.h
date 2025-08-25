5853e451fd12d3b1567e0dd56fbc4f41bf4e8f3e
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-08-25 10:05:33 +0200

    use raw energy for TICLCandidate p4

--- DataFormats/HGCalReco/interface/TICLCandidate.h
+++ DataFormats/HGCalReco/interface/TICLCandidate.h
@@ -48,7 +48,7 @@ public:
         setPdgId(pdgId * tk->charge());
         setCharge(tk->charge());
         rawEnergy_ = trackster->raw_energy();
-        auto const& regrE = trackster->regressed_energy();
+        auto const& regrE = trackster->raw_energy();
         math::XYZTLorentzVector p4(regrE * tk->momentum().unit().x(),
                                    regrE * tk->momentum().unit().y(),
                                    regrE * tk->momentum().unit().z(),
@@ -60,7 +60,7 @@ public:
         setPdgId(pdgId);
         setCharge(0);
         rawEnergy_ = trackster->raw_energy();
-        const float& regrE = trackster->regressed_energy();
+        const float& regrE = trackster->raw_energy();
         math::XYZTLorentzVector p4(regrE * trackster->barycenter().unit().x(),
                                    regrE * trackster->barycenter().unit().y(),
                                    regrE * trackster->barycenter().unit().z(),
