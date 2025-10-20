cbaa9650b227d4951b32ea9d847dc5b9809b3803
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2025-10-20 19:16:29 +0200

    Add dummy alpaka trackster producer

--- RecoLocalCalo/HGCalRecAlgos/plugins/alpaka/HGCalRecHitCalibrationAlgorithms.dev.cc
+++ RecoLocalCalo/HGCalRecAlgos/plugins/alpaka/HGCalRecHitCalibrationAlgorithms.dev.cc
@@ -240,7 +240,8 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
                                   HGCalSoARecHitsDeviceCollection::ConstView recHits,
                                   double k_noise = 0.) const {
       for (auto idx : uniform_elements(acc, recHits.metadata().size())) {
-        if (!recHits[idx].flags() && recHits[idx].energy() > k_noise * recHits[idx].sigmaNoise() && recHits[idx].layer() > 0)
+        if (!recHits[idx].flags() && recHits[idx].energy() > k_noise * recHits[idx].sigmaNoise() &&
+            recHits[idx].layer() > 0)
           sidx[alpaka::atomicAdd(acc, nsel, 1)] = idx;
       }
     }
