b3c612bb725e28305353f6f4bcda2d4716f4d065
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2026-02-18 11:33:54 +0100

    add MIP energy to LCs

--- DataFormats/HGCalReco/interface/HGCalSoAClusters.h
+++ DataFormats/HGCalReco/interface/HGCalSoAClusters.h
@@ -10,6 +10,7 @@ GENERATE_SOA_LAYOUT(HGCalSoAClustersLayout,
                     SOA_COLUMN(float, y),
                     SOA_COLUMN(float, z),
                     SOA_COLUMN(float, energy),
+                    SOA_COLUMN(float, mipEnergy),
                     SOA_COLUMN(int, cells),  // number of hits in the cluster
                     SOA_COLUMN(int, seed)    // This is the index of the seed of each cluster inside the RecHit SoA
 )
