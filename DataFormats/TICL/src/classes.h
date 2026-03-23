8f95b598d798a0536e4b1c2fba7655ad3bb76502
Wahid Redjeb <wahid.redjeb.wr@gmail.com>
2026-03-23 15:07:35 +0100

    some fixes in xml

--- DataFormats/TICL/src/classes.h
+++ DataFormats/TICL/src/classes.h
@@ -1,4 +1,5 @@
 #include "DataFormats/TICL/interface/ClusterMask.h"
 #include "DataFormats/TICL/interface/ClusterMaskHost.h"
 #include "DataFormats/TICL/interface/AssociationMap.h"
+#include "DataFormats/TICL/interface/HitAndFraction.h"
 #include "DataFormats/TICL/interface/HitsAndFractionsHost.h"
