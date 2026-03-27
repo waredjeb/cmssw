9a4353d5707a17bcb84d30211bee0259b5bd63d0
waredjeb <wahid.redjeb@cern.ch>
2026-03-27 03:03:39 +0100

    try1

--- DataFormats/TICL/interface/HitsAndFractionsHost.h
+++ DataFormats/TICL/interface/HitsAndFractionsHost.h
@@ -2,12 +2,19 @@
 #pragma once
 
 #include "DataFormats/Portable/interface/PortableHostCollection.h"
-#include "DataFormats/TICL/interface/AssociationMap.h"
 #include "DataFormats/TICL/interface/HitAndFraction.h"
+#include "DataFormats/SoATemplate/interface/SoALayout.h"
 
 namespace ticl {
 
-  using TICLAssociationMap_t = AssociationMapLayout<int, HitAndFraction>::Layout<128, false>;
-  using HitsAndFractionsHost = PortableHostCollection<TICLAssociationMap_t>;
+  // Simple SoA layout for hits and fractions - no custom view methods
+  // This avoids the PortableHostCollection + complex BLOCKS incompatibility
+  GENERATE_SOA_LAYOUT(HitsAndFractionsSoALayout,
+    SOA_COLUMN(int, offsets),           // Offset for each cluster
+    SOA_COLUMN(HitAndFraction, values)  // All hit-fraction pairs
+  )
+
+  using HitsAndFractionsSoA = HitsAndFractionsSoALayout<>;
+  using HitsAndFractionsHost = PortableHostCollection<HitsAndFractionsSoA>;
 
 }  // namespace ticl
